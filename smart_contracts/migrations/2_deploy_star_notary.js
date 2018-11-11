2ed36a8a52ea0926140c1b6fe4611d84c2fc6716
Masayuki Osuka <masayuki@huubap.com>
2018-11-11 20:30:39 +0800

    configuration for deploy and add the transaction result in txt file

--- smart_contracts/migrations/2_deploy_star_notary.js
+++ smart_contracts/migrations/2_deploy_star_notary.js
@@ -1,21 +1,5 @@
-var HDWalletProvider = require('truffle-hdwallet-provider');
+var Migrations = artifacts.require("./StarNotary.sol");
 
-var mnemonic = 'PASTE MNEMONIC HERE';
-
-module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: "*"
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, 'PASTE URL HERE')
-      },
-      network_id: 4,
-      gas: 4500000,
-      gasPrice: 10000000000,
-    }
-  }
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
 };
--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -12,7 +12,24 @@
  *   },
  */
 
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'beach cram loud shine notable purpose also peace beauty either either strong';
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/0586b503c35246f6a57bb3bf32f63ba8')
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
+    }
+  }
 };
